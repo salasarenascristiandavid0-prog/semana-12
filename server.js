9efe6391cfd52bcc933124275a2f936921e174d4
cris <salasarenascristiandavid0@gmail.com>
2026-06-14 23:47:41 -0500

    add: semana 9 files and express server

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Servidor activo y funcionando');
+    res.send('Servidor activo y funcionando');
 });
 
 app.get('/estado', (req, res) => {
-  res.json({
-    estado: "Servidor funcionando",
-    servicio: "API comunitaria"
-  });
+    res.json({
+        estado: "Servidor funcionando",
+        servicio: "API comunitaria"
+    });
 });
 
-app.listen(3000, () => {
-  console.log('Servidor ejecutándose en puerto 3000');
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
